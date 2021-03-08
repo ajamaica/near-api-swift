6fb259d127481a9d2948d3a5ca00d914ccc245dc
Arturo Jamaica <me@arturojamaica.com>
2021-03-08 12:04:05 +0900

    Add mock test and test target

---
+++ nearclientios/Tests/NearTests.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Arturo Jamaica on 2021/03/08.
+//
+
+import Foundation
+import XCTest
+
+class NesrTests: XCTestCase {
+
+    func testHello(){
+        XCTAssertEqual("Hello", "Hello")
+    }
+    
+}
